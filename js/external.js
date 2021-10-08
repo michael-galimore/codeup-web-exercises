d56d45275c8154691325d39a64c5e246a190548d
michael galimore <michael.galimore1@gmail.com>
2021-10-08 17:12:39 -0500

    exercises

---
+++ js/external.js
@@ -0,0 +1,25 @@
+console.log('Hello from external JavaScript.');
+alert("Welcome to my Website!");
+
+var favColor = prompt("What is your favorite color?");
+alert(favColor + " is my favorite color as well!");
+
+
+var costPerDay = 3;
+var littleMermaid = prompt("How many days are you going to rent the Little Mermaid movie?"));
+var brotherBear = prompt("How many days are you going to rent the Brother Bear movie?"));
+var hercules = prompt("How many days are you going to rent the Hercules movie?");
+var totalCost = alert(littleMermaid * 3 + brotherBear * 3 + hercules * 3 + " is your total.")
+
+
+
+var google = 400
+var amazon = 380
+var facebook = 350
+prompt("How many hour did you work at Google?")
+prompt ("How many hours did you work at Amazon?")
+prompt ("How many hours did you work at Facebook?")
+alert(google * 6 + amazon * 4 + facebook * 10 + " this how much you made for the week.")
+
+
+
