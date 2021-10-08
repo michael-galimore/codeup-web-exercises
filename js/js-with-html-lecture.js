50a5a8d1f4b33be83ac86cdc5f906a9ee8caf2cc
michael galimore <michael.galimore1@gmail.com>
2021-10-08 10:49:33 -0500

    completed the js with html lec

---
+++ js/js-with-html-lecture.js
@@ -0,0 +1,13 @@
+// "use strict";
+
+console.log("Hello, I am in my own file now");
+
+var returnOfAlert = alert("TGIF! Thank Goodness I'm Fresh");
+
+
+var returnOfConfirm = confirm("Got any plans for this three day weekend?");
+console.log("returnOfConfirm");
+
+
+var favVideoGame = prompt("What's your favorite videogame?");
+console.log("Are you serious, " + favVideoGame + " is your favorite game?");
