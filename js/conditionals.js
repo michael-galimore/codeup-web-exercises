d47f90d828e7cedccf1d1f98118a683714ea057a
michael galimore <michael.galimore1@gmail.com>
2021-10-18 11:43:34 -0500

    anwsered more

--- js/conditionals.js
+++ js/conditionals.js
@@ -21,19 +21,19 @@
  */
 
 
-function analyzeColor(color) {
-   if (color === "red") {
-       return ("Strawberries are red")
-   } else if (color === "blue") {
-       return ("The sky is blue")
-
-   } else
-       return ("I don't know about cyan")
-
-}
-console.log(analyzeColor("red"))
-console.log(analyzeColor("blue"))
-console.log(analyzeColor("cyan"))
+// function analyzeColor(color) {
+//    if (color === "red") {
+//        return ("Strawberries are red")
+//    } else if (color === "blue") {
+//        return ("The sky is blue")
+//
+//    } else
+//        return ("I don't know about " + color)
+//
+// }
+// console.log(analyzeColor("red"))
+// console.log(analyzeColor("blue"))
+// console.log(analyzeColor("cyan"))
 
 
 // Don't change the next two lines!
@@ -48,25 +48,25 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * Pass the `randomColor` variable to your function and console.log the results.
  * You should see a different message every time you refresh the page
  */
-analyzeColor = randomColor
-console.log(analyzeColor);
-// correct code
-console.log(analyzeColor(randomColor));
+
+
 /**
  * TODO:
  * Refactor your above function to use a switch-case statement
  */
-
-switch(analyzeColor){
+function analyzeColor(color){
+switch(color){
     case "red":
        alert ("Strawberries are red")
+        break;
     case "blue":
         alert("The sky is blue")
         break;
     default:
         alert("I don't know about that color")
         break;
-}
+}}
+console.log(analyzeColor(randomColor));
 
 /**
  * TODO:
@@ -74,11 +74,11 @@ switch(analyzeColor){
  * user to your `analyzeColor` function. Alert the return value from your
  * function to show it to the user.
  */
-analyzeColor = prompt("I need a color, so type in a color. Please...");
-alert("You just gave me " + analyzeColor + " as a color. Thanks!");
+// analyzeColor = prompt("I need a color, so type in a color. Please...");
+// alert("You just gave me " + analyzeColor + " as a color. Thanks!");
 
 // correct
-var UserColor = prompt("What is your favorite color?");
+var userColor = prompt("What is your favorite color?");
 alert(analyzeColor(userColor));
 
 /* ########################################################################## */
@@ -104,34 +104,44 @@ alert(analyzeColor(userColor));
  */
 // I got lost
 var luckyNumber = Math.floor(Math.random() * 5);
-var billTotal = prompt("What was your total for todat?y")
-function calculateTotal() {
-    if(luckyNumber === 0){
-        return luckyNumber + billTotal;
+var billTotal = prompt("What was your total for today?")
+function calculateTotal(luckyNum, total) {
+    if(luckyNum === 0){
+        return total;
+    } else if(luckyNum === 1){
+        return total - (total * .1);
+    } else if (luckyNum === 2){
+        return total - (total * .25);
+    } else if (luckyNum === 3){
+        return total - (total * .35)
+    } else if (luckyNum === 4){
+        return total - (total * .5)
+    } else if (luckyNum === 5){
+        return 0
     }
-    return discountedPrice;
+
 
 }
-console.log(luckyNumber);
+console.log(calculateTotal(3,100));
 
-// correct code
-function calculateTotal(luckNum, total) {
-    switch (luckyNum) {
-
-        case 1:
-            return total - (total * .1);
-        case 2:
-            return total - (total * .25);
-        case 3:
-            return total - (total * .35);
-        case 4:
-            return total - (total * .5);
-        case 5:
-            return total;
-            case 0:
-            return total;
-    }
-}
+// // correct code
+// function calculateTotal(luckNum, total) {
+//     switch (luckyNum) {
+//
+//         case 1:
+//             return total - (total * .1);
+//         case 2:
+//             return total - (total * .25);
+//         case 3:
+//             return total - (total * .35);
+//         case 4:
+//             return total - (total * .5);
+//         case 5:
+//             return total;
+//             case 0:
+//             return total;
+//     }
+// }
 
 /**
  * TODO:
@@ -162,30 +172,30 @@ alert("Your lucky number was " + luckuNumber + "\nYour total was " + userTotal +
  * Can you refactor your code to use functions?
  * HINT: The way we prompt for a value could be improved
  */
-
-function playNumberGame(num) {
-    var userNum = parseFloat(num);
-    if (!isNaN(userNum)) {
-        if (userNum & 2 === 0) {
-            alert(userNum + " is an even number.")
-        } else {
-            alert(userNum + " is an odd number")
-        }
-        alert(userNum + " plus 100 is " + (userNum + 100));
-
-        if (userNum < 0) {
-            alert(userNum + " is negative number.")
-
-        } else if (userNum > 0) {
-            alert(userNum + " is positive number.")
-        } else {
-            alert("You entered in zero")
-
-        } else {
-            alert("You didn't give me a number.")
-        }
-    }
-}
+//
+// function playNumberGame(num) {
+//     var userNum = parseFloat(num);
+//     if (!isNaN(userNum)) {
+//         if (userNum & 2 === 0) {
+//             alert(userNum + " is an even number.")
+//         } else {
+//             alert(userNum + " is an odd number")
+//         }
+//         alert(userNum + " plus 100 is " + (userNum + 100));
+//
+//         if (userNum < 0) {
+//             alert(userNum + " is negative number.")
+//
+//         } else if (userNum > 0) {
+//             alert(userNum + " is positive number.")
+//         } else if (us)
+//             alert("You entered in zero")
+//
+//         } else {
+//             alert("You didn't give me a number.")
+//         }
+//     }
+// }
 
 
 
